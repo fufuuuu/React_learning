01b55432422dc62d3368c81cef40fabfdd7f3d80
Joe <jinzeng@ebay.com>
2021-02-25 18:28:38 +0800

    A simple borad to record my click.(easy version)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # React_learning
 Document my experience in learning react
+
+<link herf = "https://zh-hans.reactjs.org/tutorial/tutorial.html#setup-option-2-local-development-environment">本地环境配置</link>
+
+配置本地环境需要确认你的node -v >= 10.16 并且你的npm -v >= 5.6
+<code>npx create-react-app my-app</code>
+
+
---
+++ my-app/src/index.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class Square extends React.Component {
+  //利用state来实现记忆的功能
+  constructor(props) {
+    super(props);
+    this.state= {
+      value: null,
+    }
+  }
+  render() {
+    return (
+      <button className="square" onClick={() => this.setState({value: 'X'})}>
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+
+  render() {
+    const status = 'Next player: X';
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+//0.0  疑惑点：组件构建手法类似于闭包？？？
+class Game extends React.Component {
+  render() {
+    return (
+      //调用模块从这里开始！！！
+      <div className="game">
+        <ShoppingList name='Mark'/>
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// 渲染生成dom树
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
